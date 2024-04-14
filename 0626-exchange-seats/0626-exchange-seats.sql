300ef98aea6d34557331584a279462a3078cc87d
Mohit Mandlewala <mandlewalamohit@gmail.com>
2024-04-13 20:16:32 -0400

    Time: 632 ms (72.88%), Space: 0B (100.00%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select case
+    when id%2=0 then id-1
+    when id%2=1 and id+1 not in (select id from seat) then id
+    else id+1 end as id,
+    student
+from seat
+order by id
+;
